b10151e4d433593ac0b4aa7153927e9f64035431
nicolassmith75 <nicolassmith75@yahoo.com>
2025-11-21 21:46:43 -0600

    ___init__
    I added the __init__ def that allows the program to call upon them for status, if its muted, the volume.

--- television.py
+++ television.py
@@ -4,3 +4,10 @@ class Television:
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
+
+    def __init__(self):
+        self._status = False # Turns power off
+        self._muted = False  # Shows not muted
+        self._volume = self.MIN_VOLUME # min volume is 2
+        self._channel = self.MIN_CHANNEL # min channel is 0
+        self._previous_volume = self._volume  # previous volume sync
