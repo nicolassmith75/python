6053b03e4e69f260f48a23907b0a1ccd4af2144e
nicolassmith75 <nicolassmith75@yahoo.com>
2025-11-21 21:50:33 -0600

    Added power and mute variables
    Created the def's for the power and the mute options for the tv

--- television.py
+++ television.py
@@ -11,3 +11,15 @@ class Television:
         self._volume = self.MIN_VOLUME # min volume is 2
         self._channel = self.MIN_CHANNEL # min channel is 0
         self._previous_volume = self._volume  # previous volume sync
+
+    def power(self):
+        self._status = not self._status # the status of if the tv is on or off
+
+    def mute(self):
+        if self._status:
+            self._muted = not self._muted # checks the mute
+            if self._muted:
+                self._previous_volume = self._volume  # remembers what volume number before muting
+                self._volume = 0
+            else:
+                self._volume = self._previous_volume # restores the volume that was remembered
